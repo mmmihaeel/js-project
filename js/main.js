895cb2295761a33860b8d072c2b24d7a182362d9
mmmihhaeel <mikael11032005@gmail.com>
2022-06-02 19:55:43 +0300

    third practise

--- js/main.js
+++ js/main.js
@@ -1,131 +1,78 @@
 /* Задание на урок:
-
-1) Создать переменную numberOfFilms и в неё поместить ответ от пользователя на вопрос:
-'Сколько фильмов вы уже посмотрели?'
-
-2) Создать объект personalMovieDB и в него поместить такие свойства:
-    - count - сюда передается ответ на первый вопрос
-    - movies - в это свойство поместить пустой объект
-    - actors - тоже поместить пустой объект
-    - genres - сюда поместить пустой массив
-    - privat - в это свойство поместить boolean(логическое) значение false
-
-3) Задайте пользователю по два раза вопросы:
-    - 'Один из последних просмотренных фильмов?'
-    - 'На сколько оцените его?'
-Ответы стоит поместить в отдельные переменные
-Записать ответы в объект movies в формате: 
-    movies: {
-        'logan': '8.1'
-    }
-
-Проверить, чтобы все работало без ошибок в консоли */
+1) Первую часть задания повторить по уроку
+2) Создать функцию showMyDB, которая будет проверять свойство privat. Если стоит в позиции
+false - выводит в консоль главный объект программы
+3) Создать функцию writeYourGenres в которой пользователь будет 3 раза отвечать на вопрос 
+"Ваш любимый жанр под номером ${номер по порядку}". Каждый ответ записывается в массив данных
+genres
+P.S. Функции вызывать не обязательно */
 
 'use strict';
 
-const numberOfFilms = promt("Скільки фільмів ви вже переглянули", ""),
-    personalMovieDB = {
+
+let numberOfFilms;
+const personalMovieDB = {
         count: numberOfFilms,
         movies: {},
         actors: {},
         genres: [],
         privat: false,
-    };
+};
 
-// for (let i = 1; i < 3; i++) {
-//     const a = promt("Один з останніх фільмів", ""),
-//         b = promt("На скільки оціните його", "");
-
-//     if (a != '' && b != '' && a != null && b != null && a.length < 50 ) {
-//         personalMovieDB.movies[a] = b;
-//         console.log('Всьо, молодецю сончеко, ставлю шавуху');
-//     } else {
-//         console.log('Валєра заїбав, введи нормально дані');
-//         i--;        
-//     }
-
-// }
-
-
-if (personalMovieDB.count <= 10) {
-    console.log("Просмотрено довольно мало фильмов");
-} else if (personalMovieDB.count > 10 && personalMovieDB.count <= 30) {
-    console.log("Вы классический зритель");
-} else if (personalMovieDB.count > 30) {
-    console.log("Вы киноман");
-} else {
-    console.log("Произошла ошибка");
-}
-
-
-let i = 1;
-while (i < 3) {
-    const a = promt("Один з останніх фільмів", ""),
-        b = promt("На скільки оціните його", "");
-
-    i++;
-
-    if (a != '' && b != '' && a != null && b != null && a.length < 50) {
-        personalMovieDB.movies[a] = b;
-        console.log('Всьо, молодецю, ставлю шавуху');
-    } else {
-        console.log('Валєра заїбав, введи нормально дані');
-        i--;
-    }
-}
-
-
-
-/* Задание на урок:
-1) Автоматизировать вопросы пользователю про фильмы при помощи цикла
-2) Сделать так, чтобы пользователь не мог оставить ответ в виде пустой строки,
-отменить ответ или ввести название фильма длинее, чем 50 символов. Если это происходит - 
-возвращаем пользователя к вопросам опять. (К любой строке можно обратиться как 
-str.length - и получить её длину)
-3) При помощи условий проверить  personalMovieDB.count, и если он меньше 10 - вывести сообщение
-"Просмотрено довольно мало фильмов", если от 10 до 30 - "Вы классический зритель", а если больше - 
-"Вы киноман". А если не подошло ни к одному варианту - "Произошла ошибка"
-4) Потренироваться и переписать цикл еще двумя способами*/
-
-
-
-
-
-
-function returnNeighboringNumbers(number) {
-    let arr = [];
-    for (let i = 0; i < 3; i++) {
-        arr[i] = number - 1 + i;
-    }
-
-    return arr;
-}
-console.log(returnNeighboringNumbers(10));
-
-
-
-
-
-function getMathResult(a, b) {
-    let result = '';
-
-    if (typeof (b) != 'number') { 
-        result += `${a}`;
-    } else if ( b <= 1) {
-         result += `${a}`;
-    } else {
-        for (let i = 1; i <= b; i++) {
-            result += `${i * a}`;
-            if (i * a === a * b) {
-                break;
-            } else {
-                result += `---`;
-            }
-        }
-    }
-
-    return result;
+function start() {
+    numberOfFilms = +prompt("Скільки фільмів ви вже переглянули", "");
     
+    while (numberOfFilms == '' || numberOfFilms == null || isNaN(numberOfFilms)) {
+        numberOfFilms = +prompt("Скільки фільмів ви вже переглянули", "");
+    }
+
+}
+start();
+
+function rememberMyFilms() {
+    for (let i = 1; i < 3; i++) {
+        const a = prompt("Один з останніх фільмів", ""),
+            b = prompt("На скільки оціните його", "");
+
+        if (a != '' && b != '' && a != null && b != null && a.length < 50) {
+            personalMovieDB.movies[a] = b;
+            console.log('Всьо, молодецю сончеко, ставлю шавуху');
+        } else {
+            console.log('Валєра заїбав, введи нормально дані');
+            i--;
+        }
+
+    }
+}
+rememberMyFilms();
+
+function detectPersonalLevel() {
+
+    if (personalMovieDB.count <= 10) {
+        console.log("Просмотрено довольно мало фильмов");
+    } else if (personalMovieDB.count > 10 && personalMovieDB.count <= 30) {
+        console.log("Вы классический зритель");
+    } else if (personalMovieDB.count > 30) {
+        console.log("Вы киноман");
+    } else {
+        console.log("Произошла ошибка");
+    }
+}
+detectPersonalLevel();
+
+
+function showMyDB (hidden) {
+    if (!hidden) {
+        console.log(personalMovieDB);
+    }
 }
 
-console.log(getMathResult(3, 10));
+showMyDB(personalMovieDB.privat);
+
+function writeYourGenres() {
+    for (let i = 1; i <= 3; i++) {
+        personalMovieDB.genres[i - 1] = prompt(`Ваш любимый жанр под номером ${i}`);
+    }
+}
+
+writeYourGenres();
--- js/script.js
+++ js/script.js
@@ -1,3 +1,39 @@
-console.log(8 % 4)
+console.log(8 % 4);
 
 //я нахуй фронтендєр
+
+function returnNeighboringNumbers(number) {
+    let arr = [];
+    for (let i = 0; i < 3; i++) {
+        arr[i] = number - 1 + i;
+    }
+
+    return arr;
+}
+console.log(returnNeighboringNumbers(10));
+
+
+
+
+
+function getMathResult(a, b) {
+    let result = '';
+
+    if (typeof (b) !== 'number' || b <= 1) { 
+        return a;
+    } else {
+        for (let i = 1; i <= b; i++) {
+            result += `${i * a}`;
+            if (i * a === a * b) {
+                break;
+            } else {
+                result += `---`;
+            }
+        }
+    }
+
+    return result;
+    
+}
+
+console.log(getMathResult(3, `1`));
